d2801cb6d7c996be2b9c592d815f3f7f393fa90e
zuhairnasser <alrkabi1992@gmail.com>
2021-01-15 23:56:49 +0200

    create login controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,80 @@
+<?php
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Laravel\Passport\Client;
+use Carbon\Carbon;
+// use GuzzleHttp\Client;
+class LoginController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api')->only('logout');
+    }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    
+    public function alluser(Request $request){
+        $user = User::all();
+        return response()->json(['user' => $user], 200);
+    }
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string', 'min:8'],
+        ]);
+        $credentials = request(['email', 'password']);
+        if(!Auth::attempt($credentials)){
+            return response()->json([
+                'errors' => [
+                    'email' => ['could not sign you in with those credentials']
+                ]
+            ], 422);
+        }
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+        if ($request->remember_me){
+            $token->expires_at = Carbon::now()->addWeeks(1);
+        }
+        $token->save();
+        return response()->json([
+            'data' => $user,
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateTimeString()
+        ]);
+    }
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+        $user = User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+        if(!$user){
+            return response()->json(['success' => false, 'message'=> 'Registration failed']);
+        }
+        return response()->json(['success' => true, 'message'=> 'Registration successful']);
+    }
+}
