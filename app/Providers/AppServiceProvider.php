9132c804f40f27b03981ae2540fa42e1869999a7
zuhairnasser <alrkabi1992@gmail.com>
2021-01-15 23:47:14 +0200

    no thing new

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Database\Schema\Builder;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Builder::defaultStringLength(191);
     }
 }
