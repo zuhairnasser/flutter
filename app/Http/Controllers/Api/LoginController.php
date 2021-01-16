a3675b22eb304b2b3080ca63a3788a21b489609b
zuhairnasser <alrkabi1992@gmail.com>
2021-01-16 09:54:42 +0200

    add login function

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -11,7 +11,32 @@ use Carbon\Carbon;
 // use GuzzleHttp\Client;
 class LoginController extends Controller
 {
-   
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        
+        ]);
+        $credentials = request(['email', 'password']);
+        if(!Auth::attempt($credentials))
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+        if ($request->remember_me)
+            $token->expires_at = Carbon::now()->addWeeks(1);
+        $token->save();
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateTimeString()
+        ]);
+    }
 
     public function register(Request $request)
     {
